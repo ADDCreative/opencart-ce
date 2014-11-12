768682f3e2650dd11b32ff4ff369c9b290e47980
John Sheppard <john@addevelopments.com>
2014-11-12 19:32:12 +0000

    Update encryption
    Use MCRYPT_RIJNDAEL_128 and MCRYPT_MODE_CBC. Generate IV with
    secure_random_bytes for every encription. Add PKCS7 padding.

--- upload/system/library/encryption.php
+++ upload/system/library/encryption.php
@@ -5,15 +5,35 @@ final class Encryption {
 
 	public function __construct($key) {
 		$this->key = hash('sha256', $key, true);
-		$this->iv = mcrypt_create_iv(32, MCRYPT_RAND);
 	}
 
 	public function encrypt($value) {
-		return strtr(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->key, $value, MCRYPT_MODE_ECB, $this->iv)), '+/=', '-_,');
+		$this->iv = secure_random_bytes(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC));
+		$block_size = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+
+		$pad = $block_size - (strlen($value) % $block_size);
+		$data = $value . str_repeat(chr($pad), $pad);
+
+		return strtr(base64_encode($this->iv . mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $this->key, $data, MCRYPT_MODE_CBC, $this->iv)), '+/=', '-_,');
 	}
 
 	public function decrypt($value) {
-		return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $this->key, base64_decode(strtr($value, '-_,', '+/=')), MCRYPT_MODE_ECB, $this->iv));
+		$value = base64_decode(strtr($value, '-_,', '+/='));
+		$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+
+		$iv = substr($value, 0, $iv_size);
+		$data = substr($value, $iv_size);
+
+		$data = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $this->key, $data, MCRYPT_MODE_CBC, $iv);
+
+		if (strlen($data)) {
+			$pad = ord($data[strlen($data) - 1]);
+			$data = substr($data, 0, -$pad);
+
+			return $data;
+		} else {
+			return false;
+		}
 	}
 }
 ?>
